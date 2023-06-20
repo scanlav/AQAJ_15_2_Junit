b547fd84fbf4ffbc30a582c21fbed4cb95b6b763
Viktor Churikov <scanlave@gmail.com>
2023-06-21 01:16:42 +0300

    Сборка проекта

--- src/test/java/exercise/article/WorkerImplTest.java
+++ src/test/java/exercise/article/WorkerImplTest.java
@@ -15,6 +15,7 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
 
+@DisplayName("Тесты работника")
 class WorkerImplTest {
     private WorkerImpl worker;
 
